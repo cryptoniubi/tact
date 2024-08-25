16e06f9c2dab71194c21b75ffd322dae7777055d
Mohammadreza Mohammadi <92263217+cryptoniubi@users.noreply.github.com>
2024-08-25 08:04:42 -0700

    Update jest.config.js
    better this way

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-    maxWorkers: "50%",
+    maxWorkers: "70%",
     globalSetup: "./jest.setup.js",
     globalTeardown: "./jest.teardown.js",
     snapshotSerializers: ["@tact-lang/ton-jest/serializers"],
